27ffeeaf867a0d588d35a457f79903ff5470cba6
harshini-mv <manniharshini@gmail.com>
2026-06-07 00:03:41 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://minisocial-backend-9zed.onrender.com/auth/login",
+  baseURL: "https://minisocial-backend-9zed.onrender.com/api",
 });
 
 export default API;
